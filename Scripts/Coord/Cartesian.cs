06d924704c0ef5fd0add4673efd9c7da7b742ee3
PhileasL <phileas.lambert@gmail.com>
2020-11-18 17:06:01 +0100

    Hi Cartesian

--- Scripts/Coord/Cartesian.cs
+++ Scripts/Coord/Cartesian.cs
@@ -6,7 +6,34 @@ namespace Scripts.Coord
 {
     public class Cartesian
     {
+
+        /// <summary>
+        /// Coordinates of a Cartesian instance
+        /// </summary>
         public int x, y;
+
+        /// <summary>
+        /// Cartesian is a custructor of a cartesian system coordinate
+        /// </summary>
+        /// <param name="xPos"></param>
+        /// <param name="yPos"></param>
+        public Cartesian(int xPos, int yPos)
+        {
+            x = xPos;
+            y = yPos;
+        }
+
+        /// <summary>
+        /// Coord is a function that returns x or y depending on the given argument
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns>x or y coordinate, null if c isn't valid</returns>
+        public int Coord(int c)
+        {
+            if (c == 0) { return x; }
+            else if (c == 1) { return y; }
+            else { return null; }
+        }
     }
 }
 
--- Scripts/LightOut.cs
+++ Scripts/LightOut.cs
@@ -6,7 +6,6 @@ namespace Scripts
 {
     public class LightOut : MonoBehaviour
     {
-        public
         // Start is called before the first frame update
         void Start()
         {
