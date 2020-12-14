fca801cd30c12a891e7c12b50ad2d2b8b94e019e
PhileasL <phileas.lambert@gmail.com>
2020-12-14 21:39:13 +0100

    Hello pauseMenu

---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject lightOutGame;
+
+    public GameObject pauseMenu;
+
+    public static bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                pauseMenu.SetActive(false);
+                lightOutGame.SetActive(true);
+            } else
+            {
+                pauseMenu.SetActive(true);
+                lightOutGame.SetActive(false);
+            }
+            isPaused = !isPaused;
+        }
+    }
+}
