81d6b1635298d4234aeebc2ef8a428ef4bdb8a32
PhileasL <phileas.lambert@gmail.com>
2020-12-22 17:56:59 +0100

    Scripts definition linked to Tests

--- Scripts/Tests/Coord/TestCartesian.cs
+++ Scripts/Tests/Coord/TestCartesian.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using Scripts.Coord;
 
-namespace Tests
+namespace Scripts.Tests.Coord
 {
     public class TestCartesian
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void TestCartesianSimplePasses()
+        public void TestCartesianCoordMethod()
         {
-            // Use the Assert class to test conditions
+            float x = 1,
+                y = 2;
+            Cartesian toTest = new Cartesian(x, y);
+
+            Assert.AreEqual(toTest.Coord(0), x);
+            Assert.AreEqual(toTest.Coord(1), y);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
